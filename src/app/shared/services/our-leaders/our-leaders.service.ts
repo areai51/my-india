03fe468362c5b1478c6402f21b104a4026286f96
Aakash Goel <se.aakashgoel@hotmail.com>
2016-09-11 18:13:17 +0530

    updated our-leaders-component. improved and transformed data structure, created data model, enums, interface. data-list component as a directive

--- src/app/shared/services/our-leaders/our-leaders.service.ts
+++ src/app/shared/services/our-leaders/our-leaders.service.ts
@@ -1,16 +1,60 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import BaseService from '../base.service';
+import { OurLeadersModel, IOurLeaders, OurLeadersState } from './our-leaders.model';
 
 @Injectable()
-export class OurLeadersService extends BaseService {
+export class OurLeadersService {
 
-  constructor(protected _http: Http) {
-    super(_http);
-  }
+    private _apiUrlLokSabha: string = 'https://data.gov.in/node/85987/datastore/export/json';
+    private _apiUrlRajyaSabha: string = 'https://data.gov.in/node/982241/datastore/export/json';
 
-  getAttendance(sabha): Observable<any> {
-    return this.getAPIData(sabha);
-  }
+    constructor(private _http: Http) { }
+
+    /**
+    * Utility to shorten other calls
+    * Call all function keys on passed in list
+    */
+    private _objectBasedMap(objectMap: any) {
+        return function(dataList: any, fieldList: any, refId: number) {
+            return dataList.map(function(data) {
+                return Object.keys(objectMap)
+                    .reduce(function(memo, key) {
+                        memo[key] = objectMap[key].apply(objectMap, [data, fieldList, refId]);
+                        return memo;
+                }, {});
+            });
+        };
+    }
+
+    private _simpleData = this._objectBasedMap(OurLeadersModel);
+    private _transformedData = (data: any, fields: any, refId: number) => {
+        return this._simpleData(data, fields, refId);
+    }
+
+    private _extractData = (res: Response) => {
+        let rsData = res.json();
+        return rsData || {};
+    }
+
+    private _handleError = (error: any) => {
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+    }
+
+    getAttendance(sabhaStateId: number): Observable<any> {
+        let apiUrl = (sabhaStateId === OurLeadersState.RAJYASABHA) ? this._apiUrlRajyaSabha : this._apiUrlLokSabha;
+
+        return this._http.get(apiUrl)
+            .map(this._extractData)
+            .map((response: IOurLeaders) => {
+                let result = this._transformedData(response.data, response.fields, sabhaStateId);
+                // console.log('result---->> \n', result);
+                return result;
+            })
+            .catch(this._handleError);
+    }
+
 }
