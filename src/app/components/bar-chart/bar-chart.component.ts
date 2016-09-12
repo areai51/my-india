dfb61b3e646aede2f7b9e9db37532776748327be
Aakash Goel <Aakash-Goel@users.noreply.github.com>
2016-09-12 23:45:09 +0530

    Create state-crimes service and data-model, implement bar-chart, updated crime-in-the-city component (#14)
    +1

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -1,16 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
+// @Todo: issue when trying to load using es6 import statement
+let Chart = require('chart.js/src/chart.js');
 
 @Component({
-  selector: 'app-bar-chart',
+  selector: 'bar-chart',
   templateUrl: 'bar-chart.component.html',
   styleUrls: ['bar-chart.component.css']
 })
 export class BarChartComponent implements OnInit {
+  chartWrapper: any;
+  barChart: any;
 
-  constructor() {
+  @Input('title') chartTitle: string;
+  @Input('titleDisplay') chartTitleDisplay: boolean;
+  @Input('dataSets') chartDataSets: Array<any>;
+  @Input('dataLabels') chartDataLabels: Array<any>;
+
+  constructor(private _elm: ElementRef) {
+  }
+
+  private _initBarChartOptions = () => {
+    Chart.defaults.global.elements.rectangle.borderWidth = 0;
+    Chart.defaults.global.maintainAspectRatio = false;
   }
 
   ngOnInit() {
+    let selector = this._elm.nativeElement;
+    this.chartWrapper = selector.getElementsByClassName('bar-chart');
+  }
+
+  waitingForChartData() {
+    this._initBarChartOptions();
+    if (this.chartDataLabels.length > 0) {
+      this.createBarChart();
+    }
+  }
+
+  createBarChart = () => {
+    let ctx = this.chartWrapper.barChart.getContext('2d');
+    this.barChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.chartDataLabels[0],
+        datasets: this.chartDataSets
+      },
+      options: {
+        title: {
+          display: true,
+          text: this.chartTitle
+        },
+        legend: {
+          position: 'bottom',
+          labels: {
+            fontColor: 'rgb(217, 171, 114)',
+            boxWidth: 20
+          }
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            categoryPercentage: 1,
+            barPercentage: 0.5
+          }]
+        }
+      }
+    });
   }
 
 }
